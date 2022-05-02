4d026575289844064f3b2f36735bbefb0dfb0ca2
Pascal Khuu <pascal.khuu@orange.fr>
2022-05-02 18:51:07 +0200

    first commit back-end mastershopping

---
+++ mastershopping-api/src/main/java/co/simplon/mastershopping/MastershoppingApiApplication.java
@@ -0,0 +1,13 @@
+package co.simplon.mastershopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MastershoppingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MastershoppingApiApplication.class, args);
+	}
+
+}
