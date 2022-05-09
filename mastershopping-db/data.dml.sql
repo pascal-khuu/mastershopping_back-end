85a746163a30a073ccea45720917340acd443dcc
Pascal Khuu <pascal.khuu@orange.fr>
2022-05-09 21:40:12 +0200

    Create table BRANDS and SIZES, FOREIGN KEY for table products and new insert in data.dml.sql

--- mastershopping-db/data.dml.sql
+++ mastershopping-db/data.dml.sql
@@ -1,5 +1,8 @@
 
-INSERT INTO CATEGORIES (category_name) values ('trousers');
+INSERT INTO CATEGORIES (category_name) values ('Trousers'), ('Jeans'),('T-shirt'),('Tie'),('hat'),('gloves');
+INSERT INTO BRANDS (brand_name) values ('ZARA'), ('TIMBERLAND'), ('COACH');
+INSERT INTO SIZES (size_name) values ('S'),('XS'),('M'),('L'),('XL'),('XXL');
+INSERT INTO PRODUCTS (product_name,price,fabrics,number_stock,categories_id,brands_id,sizes_id) values ('Jean de Lorrenzo',50.00,'coton',5,1,1,1);
 INSERT INTO ROLES (role_name) values ('ROLE_ADMIN');
-INSERT INTO USERS (user_name,password,roles_id) values ('pascal.khuu@gmail.com', '123',1),('henry.khuu@gmail.com','123',1);
-INSERT INTO USERS (user_name,password) values ('mahomed.machel@gmail.com', '123');
+INSERT INTO USERS (user_name,password,roles_id) values ('pascal.khuu@gmail.com', '$2a$10$ou7/KwnnMYeWA4LSbtBSWeZ73pnhg3m1hADKbI3zC4Bj2YZKHjfnS',1),('henry.khuu@gmail.com','$2a$10$hWGbG0L9zleBP6oPpUC/4uPlYpQqSvBVld.JE8czabIZnIegPwLQ.',1);
+INSERT INTO USERS (user_name,password) values ('paul.henry@gmail.com','$2a$10$wW5cM4gEqIQcqf0fCOYYuu9mFAO3CvOYQVDIxvuNMpcsDdIzdS2mS');
--- mastershopping-db/schema.ddl.sql
+++ mastershopping-db/schema.ddl.sql
@@ -4,41 +4,51 @@
 
 CREATE TABLE CATEGORIES (
 	id SERIAL PRIMARY KEY,
-	category_name VARCHAR (30) NOT NULL
+	category_name VARCHAR (255) UNIQUE NOT NULL
 	
 );
 
 CREATE TABLE BRANDS (
 	id SERIAL PRIMARY KEY,
-	brand_name VARCHAR(30) NOT NULL
+	brand_name VARCHAR(255) UNIQUE NOT NULL
+);
+
+CREATE TABLE SIZES (
+	id SERIAL PRIMARY KEY,
+	size_name VARCHAR (255) UNIQUE NOT NULL
 );
 
 
 
 CREATE TABLE PRODUCTS (
 	id SERIAL PRIMARY KEY,
-	product_name VARCHAR (30) NOT NULL,
+	product_name VARCHAR (255) UNIQUE NOT NULL,
 	price DECIMAL NOT NULL,
-	fabrics VARCHAR (30) NOT NULL,
+	fabrics VARCHAR (255) NOT NULL,
 	number_stock INTEGER NOT NULL,
 	categories_id INTEGER NOT NULL,
 	brands_id INTEGER NOT NULL,
+	sizes_id INTEGER NOT NULL,
 	CONSTRAINT fk_categories_id
     FOREIGN KEY (categories_id)
     REFERENCES categories(id),
     CONSTRAINT fk_brands_id
     FOREIGN KEY (brands_id)
-    REFERENCES brands(id)
+    REFERENCES brands(id),
+     CONSTRAINT fk_sizes_id
+    FOREIGN KEY (sizes_id)
+    REFERENCES sizes(id)
+    
 );
 
 CREATE TABLE ROLES (
 	id SERIAL PRIMARY KEY,
-	role_name VARCHAR(30) NOT NULL
+	role_name VARCHAR(50) UNIQUE NOT NULL
 );
 
 CREATE TABLE USERS (
 	id SERIAL PRIMARY KEY,
-	user_name VARCHAR (30) NOT NULL,
+	user_name VARCHAR (255) NOT NULL,
 	password VARCHAR(60) NOT NULL,
 	roles_id INTEGER,
 	CONSTRAINT fk_roles_id
