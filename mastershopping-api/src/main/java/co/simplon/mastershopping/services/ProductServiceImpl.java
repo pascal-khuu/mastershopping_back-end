9938d803dac473f8e75504a114ef4c308cce15ad
Pascal Khuu <pascal.khuu@orange.fr>
2022-05-20 18:17:38 +0200

    order by productName for getProductUpdate

--- mastershopping-api/src/main/java/co/simplon/mastershopping/services/ProductServiceImpl.java
+++ mastershopping-api/src/main/java/co/simplon/mastershopping/services/ProductServiceImpl.java
@@ -103,7 +103,7 @@ public class ProductServiceImpl implements ProductService {
 
 	@Override
 	public List<ProductUpdateView> getProductUpdate() {
-		return products.findAllProjectedBy(ProductUpdateView.class);
+		return products.findAllProjectedByOrderByProductNameAsc(ProductUpdateView.class);
 	}
 
 	@Transactional
