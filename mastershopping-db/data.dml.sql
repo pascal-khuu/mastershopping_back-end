cb4570db5b78a8f2a5203e0ed49a3a42f6879e79
Pascal Khuu <pascal.khuu@orange.fr>
2022-06-03 16:08:42 +0200

    add luxury brand for data of brands and delete brands:GAP and GO SPORT

--- mastershopping-db/data.dml.sql
+++ mastershopping-db/data.dml.sql
@@ -1,5 +1,5 @@
 INSERT INTO CATEGORIES (category_name) values ('Pantalon'), ('Jeans'),('T-shirt'),('Echarpe'),('Chapeau'),('Bonnet');
-INSERT INTO BRANDS (brand_name) values ('ZARA'), ('TIMBERLAND'), ('COACH'),('GAP'),('GO SPORT');
+INSERT INTO BRANDS (brand_name) values ('ZARA'), ('TIMBERLAND'),('COACH'),('MICHAEL KORS'),('GIVENCHY');
 INSERT INTO SIZES (size_name) values ('S'),('XS'),('M'),('L'),('XL'),('XXL');
 INSERT INTO FABRICS (fabrics_name) values ('Coton'), ('Laine'),('Soie'), ('Paille');
 INSERT INTO PRODUCTS (product_name,picture_url,price,number_stock,categories_id,brands_id,sizes_id, fabrics_id) values ('Jean de Lorrenzo','assets/products/Jeans.png',50.00,5,2,1,1,1), ('Pantalon de noisette','assets/products/Pantalon.png',78.79,7,1,3,4,2), ('Bonnet de bonheur','assets/products/Bonnet.png',35,77,6,3,1,3), ('Echarpe Le Roselle','assets/products/Echarpe.png',35.78,9,4,1,5,4), ('Le Roseau','assets/products/Tshirt.png',8.89,10,3,4,3,3), ('La Boule de Neige','assets/products/Pantalon.png', 89,91,1,5,1,1);
